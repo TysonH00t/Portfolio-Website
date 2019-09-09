3046a80803b0f7bf6cf4bfb541f4b689b4fd7b17
Tyson <tyshuotari@gmail.com>
2019-09-08 23:12:03 -0700

    added hexagon graphs

--- main.js
+++ main.js
@@ -2,10 +2,26 @@ $(document).ready(function() {
 
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
-
+            console.log($(window).scrollTop())
 
         if($(window).scrollTop() > 0 && $(window).scrollTop() < 600) {
             $(".photo").css({top: (600 - $(window).scrollTop())/3 + 'px'});
+            $(".description h2").css({opacity: 0});
+            $(".description h3").css({opacity: 0});
+            $(".description hr").css({opacity: 0});
+            $(".description p").css({opacity: 0});
+            $(".skills h2").css({opacity: 0});
+            $(".skills hr").css({opacity: 0});
+            $(".W800").css({opacity: (($(window).scrollTop() - 600)/200) });
+        }
+        if($(window).scrollTop() > 600 && $(window).scrollTop() < 800) {
+            $(".description h2").css({opacity: (($(window).scrollTop() - 600)/200) });
+            $(".description h3").css({opacity: (($(window).scrollTop() - 600)/200) });
+            $(".description hr").css({opacity: (($(window).scrollTop() - 600)/200) });
+            $(".description p").css({opacity: (($(window).scrollTop() - 600)/200) });
+            $(".skills h2").css({opacity: (($(window).scrollTop() - 600)/200) });
+            $(".skills hr").css({opacity: (($(window).scrollTop() - 600)/200) });
+            $(".W800").css({opacity: (($(window).scrollTop() - 600)/200) });
         }
 
     }); 
